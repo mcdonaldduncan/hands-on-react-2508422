6dc7ef69392d4da06a55126e1478d98306073161
Duncan McDonald <72177724+mcdonaldduncan@users.noreply.github.com>
2023-04-03 23:45:10 +0000

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 function App() {
+  const name = 'Stargazers'
+  const nameStyle = {color: "SteelBlue"}
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <img src="images/group.svg" alt="SarGazers Group" />
+        <hgroup>
+        <h1>Meet the <i style={nameStyle}>{name}</i></h1>
+        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        
+        <button className="outline" onClick={()=>alert('Button Pressed')}>Click Me</button>
+        
+        </hgroup>
+
+      </article>
+      
+    </div>
+    
   )
 }
 export default App
